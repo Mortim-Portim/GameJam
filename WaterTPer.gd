82d8f73ed0c1e47396b0643500f96bb88989f0d2
mortim-portim <kp>
2022-11-05 23:37:47 +0100

    a

--- WaterTPer.gd
+++ WaterTPer.gd
@@ -6,7 +6,6 @@ var destination = Position2D.new()
 var enabled = true
 var time_since_last = 0
 var abklingzeit = 0.2
-var Abstellplatz = Vector2(-10000,-10000)
 
 func _physics_process(delta):
 	if !enabled:
@@ -21,7 +20,7 @@ func _on_Area2D_area_entered(area):
 	player.visible = false
 	player.set_physics_process(false)
 	destination = $Area2D2.global_position
-	player.position = Abstellplatz
+	player.position = $Position2D.global_position
 	$AnimatedSprite.play("Flowing")
 
 func _on_Area2D2_area_entered(area):
@@ -31,7 +30,7 @@ func _on_Area2D2_area_entered(area):
 	player.visible = false
 	player.set_physics_process(false)
 	destination = $Area2D.global_position
-	player.position = Abstellplatz
+	player.position = $Position2D.global_position
 	$AnimatedSprite.play("Flowing", true)
 
 
